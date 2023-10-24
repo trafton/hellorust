a60b5ca4514b8197ec3ba8c872ff4c4afc53ed3c
Trafton Haynes <traftonh@gmail.com>
2023-10-24 11:58:54 -0700

    add bsp dungeon

--- src/map_builders/mod.rs
+++ src/map_builders/mod.rs
@@ -1,7 +1,9 @@
 mod simple_map;
 mod common;
+mod bsp_dungeon;
 
 use specs::World;
+use crate::map_builders::bsp_dungeon::BspDungeonBuilder;
 use crate::map_builders::simple_map::SimpleMapBuilder;
 use super::{Map, Position};
 
@@ -15,5 +17,10 @@ pub trait MapBuilder {
 }
 
 pub fn random_builder(new_depth: i32) -> Box<dyn MapBuilder> {
-    Box::new(SimpleMapBuilder::new(new_depth))
+    let mut rng = rltk::RandomNumberGenerator::new();
+    let builder = rng.roll_dice(1, 2);
+    match builder {
+        1 => Box::new(BspDungeonBuilder::new(new_depth)),
+        _ => Box::new(SimpleMapBuilder::new(new_depth))
+    }
 }
