a171f9b5ba43baa36a428274016aaa0d175ba74b
Trafton Haynes <traftonh@gmail.com>
2023-10-25 11:50:28 -0700

    cellular automata maps

--- src/map_builders/mod.rs
+++ src/map_builders/mod.rs
@@ -1,9 +1,13 @@
 mod simple_map;
 mod common;
 mod bsp_dungeon;
+mod bsp_interior;
+mod cellular_automata;
 
 use specs::World;
 use crate::map_builders::bsp_dungeon::BspDungeonBuilder;
+use crate::map_builders::bsp_interior::BspInteriorBuilder;
+use crate::map_builders::cellular_automata::CellularAutomataBuilder;
 use crate::map_builders::simple_map::SimpleMapBuilder;
 use super::{Map, Position};
 
@@ -18,9 +22,13 @@ pub trait MapBuilder {
 
 pub fn random_builder(new_depth: i32) -> Box<dyn MapBuilder> {
     let mut rng = rltk::RandomNumberGenerator::new();
-    let builder = rng.roll_dice(1, 2);
+    let builder = rng.roll_dice(1, 4);
     match builder {
         1 => Box::new(BspDungeonBuilder::new(new_depth)),
-        _ => Box::new(SimpleMapBuilder::new(new_depth))
+        2 =>  Box::new(BspInteriorBuilder::new(new_depth)),
+        3 => Box::new(CellularAutomataBuilder::new(new_depth)),
+        _ => Box::new(SimpleMapBuilder::new(new_depth)),
     }
+
+
 }
